8960c6746d568784076c72209073d79902ba1689
bn-tw2020 <skaxodn97@gmail.com>
2022-07-27 19:45:42 +0900

    feat: 데이터 생성
    - 프로젝트에 활용되는 데이터 생성
      - 데이터베이스에 저장되는 데이터

---
+++ app/src/main/java/com/woowa/accountbook/data/entitiy/Category.kt
@@ -0,0 +1,8 @@
+package com.woowa.accountbook.data.entitiy
+
+data class Category(
+    val id: Int,
+    val isIncome: Int,
+    val name: String,
+    val color: String
+)
---
+++ app/src/main/java/com/woowa/accountbook/data/entitiy/History.kt
@@ -0,0 +1,12 @@
+package com.woowa.accountbook.data.entitiy
+
+data class History(
+    val id: Int,
+    val money: Int,
+    val content: String,
+    val year: Int,
+    val month: Int,
+    val day: Int,
+    val category: Category,
+    val payment: Payment
+)
