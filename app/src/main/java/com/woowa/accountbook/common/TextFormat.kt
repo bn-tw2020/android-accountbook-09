7a27ada6b10d8774cafde257b67d7b97592e2457
bn-tw2020 <skaxodn97@gmail.com>
2022-07-29 19:49:49 +0900

    feat: 컴포넌트 분리
    - 양쪽 정렬 텍스트 컴포넌트 생성

--- app/src/main/java/com/woowa/accountbook/common/TextFormat.kt
+++ app/src/main/java/com/woowa/accountbook/common/TextFormat.kt
@@ -3,6 +3,7 @@ package com.woowa.accountbook.common
 import java.text.DecimalFormat
 
 fun rawToMoneyFormat(money: Int, isIncome: Int): String {
+    if (money == 0) return ""
     val decimalFormat = DecimalFormat("#,###")
     val result = decimalFormat.format(money)
     return if (isIncome == 1) result else "-$result"
--- app/src/main/java/com/woowa/accountbook/ui/component/Text.kt
+++ app/src/main/java/com/woowa/accountbook/ui/component/Text.kt
@@ -1,8 +1,7 @@
 package com.woowa.accountbook.ui.component
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
@@ -14,6 +13,8 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.woowa.accountbook.ui.theme.Green2
+import com.woowa.accountbook.ui.theme.Purple
+import com.woowa.accountbook.ui.theme.Red
 import com.woowa.accountbook.ui.theme.White
 
 @Composable
@@ -36,6 +37,22 @@ fun LabelText(
     }
 }
 
+@Composable
+fun BothSideText(
+    leftText: @Composable () -> Unit,
+    rightText: @Composable () -> Unit
+) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 8.dp, horizontal = 16.dp),
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        leftText()
+        rightText()
+    }
+}
+
 @Preview(showBackground = true)
 @Composable
 private fun LabelTextPreview() {
@@ -43,5 +60,26 @@ private fun LabelTextPreview() {
         text = "여가/문화",
         textStyle = MaterialTheme.typography.caption,
         color = Green2
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun BothSideTextPreview() {
+    BothSideText(
+        leftText = {
+            Text(
+                text = "이번 달 총 지출 금액",
+                style = MaterialTheme.typography.subtitle2,
+                color = Purple
+            )
+        },
+        rightText = {
+            Text(
+                text = "834,640",
+                style = MaterialTheme.typography.subtitle2,
+                color = Red
+            )
+        }
     )
 }
