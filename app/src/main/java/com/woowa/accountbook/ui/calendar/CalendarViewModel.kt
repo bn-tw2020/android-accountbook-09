07270a91694d670e4864216c78d74ea25341eacc
bn-tw2020 <skaxodn97@gmail.com>
2022-08-01 00:07:29 +0900

    feat: 달력 화면 구성
    - 달력 데이터 공유
      - CalendarViewModel, HistoryViewModel 공유
    
    - 달력 해당 월 일 구하기
      - 해당 월의 일수 리스트 구하기
    
    - 달력에 뿌려질 데이터 생성
      - 지출, 수입, 년, 월, 일, 색상

--- app/src/main/java/com/woowa/accountbook/ui/calendar/CalendarViewModel.kt
+++ app/src/main/java/com/woowa/accountbook/ui/calendar/CalendarViewModel.kt
@@ -1,6 +1,10 @@
 package com.woowa.accountbook.ui.calendar
 
 import androidx.lifecycle.ViewModel
+import com.woowa.accountbook.data.entitiy.History
+import com.woowa.accountbook.domain.entity.DateItem
+import com.woowa.accountbook.ui.theme.LightPurple
+import com.woowa.accountbook.ui.theme.Purple
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -15,6 +19,9 @@ class CalendarViewModel @Inject constructor(private val calendar: CustomCalendar
     private val _yearMonthPair = MutableStateFlow(calendar.yearMonthPair)
     val yearMonthPair: StateFlow<Pair<Int, Int>> get() = _yearMonthPair
 
+    private val _dateList = MutableStateFlow<List<DateItem>>(emptyList())
+    val dateList: StateFlow<List<DateItem>> get() = _dateList
+
     fun setYearAndMonth(year: Int, month: Int) {
         _yearAndMonth.value = calendar.setYearAndMonth(year, month)
         _yearMonthPair.value = calendar.yearMonthPair
@@ -28,5 +35,51 @@ class CalendarViewModel @Inject constructor(private val calendar: CustomCalendar
     fun nextYearAndMonth() {
         _yearAndMonth.value = calendar.nextYearAndMonth()
         _yearMonthPair.value = calendar.yearMonthPair
+    }
+
+    fun getAllDate(histories: List<History>) {
+        val dateItems = mutableListOf<DateItem>()
+        calendar.dateList.forEachIndexed { index, pair ->
+            if (pair.first < 0) {
+                dateItems.add(DateItem(null, null, 0, 0, pair.second, LightPurple))
+            } else if (pair.first > 0) {
+                dateItems.add(DateItem(null, null, 0, 0, pair.second, LightPurple))
+            } else {
+                val groupHistory = histories.groupBy { it.day }
+                val dayList = groupHistory[pair.second]
+
+                if (dayList == null) {
+                    dateItems.add(
+                        DateItem(
+                            expense = null,
+                            income = null,
+                            year = _yearMonthPair.value.first,
+                            month = _yearMonthPair.value.second,
+                            date = pair.second,
+                            color = Purple
+                        )
+                    )
+                } else {
+                    val income = dayList
+                        .filter { it.category.isIncome == 1 }
+                        .sumOf { it.money }
+                    val expense = dayList
+                        .filter { it.category.isIncome == 0 }
+                        .sumOf { it.money }
+
+                    dateItems.add(
+                        DateItem(
+                            expense = expense,
+                            income = income,
+                            year = _yearMonthPair.value.first,
+                            month = _yearMonthPair.value.second,
+                            date = pair.second,
+                            color = Purple
+                        )
+                    )
+                }
+                _dateList.value = dateItems
+            }
+        }
     }
 }
